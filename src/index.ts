a9b4eacb3d2dc22f29810367c6e291b85fdb8784
Johnny Magrippis <j@magrippis.com>
2023-10-18 14:38:53 +0200

    ♻️  pass inputs as environment variables

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import core from '@actions/core'
 import {run} from './main'
 
 try {
-	const [, , docsRootPath, supabaseUrl, supabaseServiceRoleKey] = Bun.argv
+	if (
+		typeof Bun.env.DOCS_ROOT_PATH !== 'string' ||
+		typeof Bun.env.PUBLIC_SUPABASE_URL !== 'string' ||
+		typeof Bun.env.SUPABASE_SERVICE_ROLE_KEY !== 'string'
+	) {
+		throw new Error('Required environment variables not set')
+	}
 
 	run({
-		docsRootPath,
-		supabaseUrl,
-		supabaseServiceRoleKey,
+		docsRootPath: Bun.env.DOCS_ROOT_PATH,
+		supabaseUrl: Bun.env.PUBLIC_SUPABASE_URL,
+		supabaseServiceRoleKey: Bun.env.SUPABASE_SERVICE_ROLE_KEY,
 	})
 } catch (err) {
 	core.setFailed(`Action failed with error: ${err}`)
